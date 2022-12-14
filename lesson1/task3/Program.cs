418af20686826921181a9dfd7782ac125a769449
ivan <ivanpanferov2016@mail.ru>
2022-12-14 20:22:43 +0300

    3 task

--- lesson1/task3/Program.cs
+++ lesson1/task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("воскресенье");
+}
