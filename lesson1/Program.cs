22d44bd88711b89f1663f31455d926d7c59bd33a
ivan <ivanpanferov2016@mail.ru>
2022-12-14 19:33:56 +0300

    init project

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+int quarter = number * number;
+Console.WriteLine(quarter);
